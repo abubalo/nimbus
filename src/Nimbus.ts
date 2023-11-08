830a3e8e4a4761465e166b78055542df253086d6
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-08 21:29:37 +0200

    chore: Make baseURL parameter in nimbus  function optional

--- src/Nimbus.ts
+++ src/Nimbus.ts
@@ -2,7 +2,7 @@ import HttpClient from "./HttpClient";
 
 let httpClientInstance: HttpClient;
 
-export default function nimbus(baseUrl: string): HttpClient {
+export default function nimbus(baseUrl?: string): HttpClient {
   if (!httpClientInstance) {
     httpClientInstance = new HttpClient(baseUrl);
   }
