b07ede499bb0ceeaa5795c3161856f4278b9197e
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-09 14:55:01 +0200

    feat: Implement custom error

---
+++ src/NimbusError.ts
@@ -0,0 +1,20 @@
+export class NimbusError<T> extends Error {
+  name: string;
+  status: number | undefined;
+  response: T | undefined;
+  constructor(message: string, status?: number, response?: T) {
+    super(message);
+
+    // Set ame of the custom error class
+    this.name = "NimbusError";
+
+    this.status = status; // HTTP status code
+    this.response = response; // Response data or any other relevant information
+
+    // Ensure the stack trace is captured
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, NimbusError);
+    }
+  }
+}
+
