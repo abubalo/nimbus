0f9e4e03c04640a96416dae618f3dd81668250e2
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-20 21:44:24 +0200

    Refactor: Enhance code documentation in type definition

--- types.d.ts
+++ types.d.ts
@@ -1,50 +1,51 @@
 import * as http from "https";
 
 /**
- * Respresent the option for making HTTP requests
+ * Represents the options for making HTTP requests.
  */
 export interface RequestOptions<T> extends http.RequestOptions {
   method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE"; // Allowed HTTP methods
-  headers?: Record<string, string>;
-  queryParameters?: Record<string, string>;
-  body?: T;
-  responseType?: "json" | "text" | "xml"; // Specify expected response format
+  headers?: Record<string, string>; // Headers for the HTTP request
+  queryParameters?: Record<string, string>; // Query parameters for the request
+  body?: T; // Request body data
+  responseType?: "json" | "text" | "xml"; // Expected response format
   contentType?:
     | "application/json"
     | "application/xml"
     | "text/plain"
-    | "multipart/form-data";
-  timeout?: number; // Timeout in milliseconds
+    | "multipart/form-data"; // Content type for the request
+  timeout?: number; // Timeout in milliseconds for the request
   onProgress?: (progress: number) => void; // Progress tracking callback
   interceptors?: {
     request?: (
       options: RequestOptions<T>
-    ) => RequestOptions<T> | Promise<RequestOptions<T>>;
+    ) => RequestOptions<T> | Promise<RequestOptions<T>>; // Request interceptor
     response?: (
       response: HttpResponse<T>
-    ) => HttpResponse<T> | Promise<HttpResponse<T>>;
+    ) => HttpResponse<T> | Promise<HttpResponse<T>>; // Response interceptor
   };
 }
 
 /**
- * Represents Structure of HTTP response
+ * Represents the structure of an HTTP response.
  */
 export interface HttpResponse<T> {
-  data: T; //Actual HTTP response data
+  data: T; // Actual HTTP response data
   status?: number; // The HTTP status code of the response
-  statusMessage?: string; // The HTTP statusMessage corresponding the status
-  headers: Record<string, any>; //An object containing the response headers sent by the server.
+  statusMessage?: string; // The HTTP statusMessage corresponding to the status
+  headers: Record<string, any>; // An object containing the response headers sent by the server
 }
 
 /**
- * Represents object that takes configurarion keys and values
+ * Represents an object containing configuration keys and values.
  */
-export interface Config {
-  baseURL: string;
-  withCredential?: boolean;
-  headers?: Record<string, unknown>;
-  transformRequest?: () => void;
-  transformResponse?: () => void;
-  timeout?: number;
-  validateStatus: number;
+export interface Config<T> {
+  baseURL: string; // Base URL for the requests
+  withCredential?: boolean; // Boolean indicating whether to include credentials in the request
+  headers?: Record<string, unknown>; // Headers for the requests
+  transformRequest?: (value: T) => T; // Transformation function for the request data
+  transformResponse?: (value: T) => T; // Transformation function for the response data
+  validateStatus: number; // HTTP status code for which the request should be considered successful
+  timeout?: (status: number) => boolean; // Timeout in milliseconds for the requests
+  responseType?: string;
 }
